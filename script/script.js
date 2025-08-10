f0bdf3802c145e9114e4b56a5988460f17284ed1
Aspin Chakma <aspinchakma2019@gmail.com>
2025-08-10 11:45:42 +0600

    search data impliment

--- script/script.js
+++ script/script.js
@@ -322,3 +322,106 @@ function showNoVideos() {
 `;
   drawiingContainer.appendChild(div);
 }
+
+// search field
+
+document.getElementById("searchField").addEventListener("keyup", (e) => {
+  const searchText = e.target.value;
+  loadDataBySearch(searchText);
+});
+
+const loadDataBySearch = async (data) => {
+  try {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/phero-tube/videos?title=${data}`
+    );
+    const getData = await res.json();
+    showSearchedData(getData.videos, data);
+    console.log(getData);
+  } catch (error) {}
+};
+
+function showSearchedData(data, search) {
+  const allCategoriesVideosContainer = document.getElementById(
+    "all_categories_video_container"
+  );
+  allCategoriesVideosContainer.innerHTML = "";
+  if (!data.length) {
+    allCategoriesVideosContainer.classList.remove("grid");
+    allCategoriesVideosContainer.classList.remove("grid-cols-4");
+    allCategoriesVideosContainer.classList.remove("gap-5");
+    const div = document.createElement("div");
+    div.innerHTML = `
+<div class="text-center mt-[9%]">
+          <img src="/Icon.png" class="mx-auto" alt="" />
+          <p class="text-4xl font-bold mt-5">
+            Oops!! Sorry, There is no content here.
+          </p>
+        </div>
+`;
+    allCategoriesVideosContainer.appendChild(div);
+  } else {
+    allCategoriesVideosContainer.classList.add("grid");
+    allCategoriesVideosContainer.classList.add("grid-cols-4");
+    allCategoriesVideosContainer.classList.add("gap-5");
+    data.forEach((video) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+    <div id="video_container" onclick="document.getElementById('my_modal_22233${
+      video.video_id
+    }').showModal()">
+          <div class="card bg-base-100 w-full shadow-xl">
+            <figure>
+              <img class='h-[200px] object-cover'
+                src="${video.thumbnail}"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="py-2 px-3 flex gap-2 items-center">
+              <div>
+                <img class="w-[40px] h-[40px] rounded-full object-cover" src="${
+                  video.authors[0].profile_picture
+                }" alt="images" />
+              </div>
+              <div>
+                <h2 class="font-bold text-xl">
+                 ${video.title}
+                </h2>
+                <h3 class="text-gray-600 flex items-center">
+                  ${video.authors[0].profile_name}
+                  ${
+                    video.authors[0].verified === true
+                      ? `<span
+                    ><img class="h-[18px] ml-4" src="/verified.png" alt=""
+                  /></span>`
+                      : ``
+                  }
+                  
+                </h3>
+                <p class="text-gray-600">${video.others.views} views</p>
+              </div>
+            </div>
+          </div>
+          <!-- Modal  -->
+          <dialog id="my_modal_22233${video.video_id}" class="modal">
+            <div class="modal-box max-w-md">
+              <p class="py-4">
+                ${video.description}
+              </p>
+              <p class="py-4">
+                Author Name: ${video.authors[0].profile_name}
+              </p>
+              <div class="modal-action">
+                <form method="dialog">
+                  <!-- if there is a button in form, it will close the modal -->
+                  <button class="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+        </div>
+    `;
+      allCategoriesVideosContainer.appendChild(div);
+    });
+  }
+}
